a9cd4b836daefb1c42346445f5d38fbd52931082
Christian <ferchris82@hotmail.com>
2024-02-04 14:41:24 -0500

    Version 1.3

--- src/main/java/org/chrisferdev/apiservlet/webapp/headers/services/ProductoService.java
+++ src/main/java/org/chrisferdev/apiservlet/webapp/headers/services/ProductoService.java
@@ -1,5 +1,6 @@
 package org.chrisferdev.apiservlet.webapp.headers.services;
 
+import org.chrisferdev.apiservlet.webapp.headers.models.Categoria;
 import org.chrisferdev.apiservlet.webapp.headers.models.Producto;
 
 import java.util.List;
@@ -8,5 +9,12 @@ import java.util.Optional;
 public interface ProductoService {
     List<Producto> listar();
     Optional<Producto> porId(Long id);
+
+    void guardar(Producto producto);
+    void eliminar(Long id);
+
+    List<Categoria> listarCategoria();
+
+    Optional<Categoria> porIdCategoria(Long id);
 
 }
